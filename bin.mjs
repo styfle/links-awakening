6bb1d29a00ef74eaebb2686f913382034a8e24df
Steven <steven@ceriously.com>
2022-12-31 17:03:46 -0500

    Many improvements

--- bin.mjs
+++
@@ -1,4 +0,0 @@
-import { check } from './index.mjs'
-
-// node ./bin.mjs 'https://styfle.dev'
-check(new URL(process.argv[2])).catch(console.error);
---
+++ src/bin.ts
@@ -0,0 +1,20 @@
+import { awaken, AwakenResult } from './index.js'
+
+// @ts-ignore
+const process = globalThis.process;
+const str = process.argv[2];
+const initUrl = new URL(str);
+const results = new Map<string, AwakenResult>();
+const onAwaken = ({ status, url, referer, msg }: AwakenResult) => {
+  if (200 <= status && status <= 299) {
+    console.log(`✅ ${url}`);
+  } else {
+    console.log(`❌ ${url} (status: ${msg ?? status}, referer: ${referer})`);
+  }
+}
+const start = Date.now();
+await awaken({ url: initUrl, onAwaken, results });
+const end = Date.now();
+const seconds = (end - start) / 1000;
+console.log(`Done! Awakened ${results.size} links in ${seconds} seconds`);
+process.exit(0); // TODO: should this exit with 1 if there were errors?
