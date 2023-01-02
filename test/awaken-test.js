526e0267f370b4e43768ea324f1a71076e4c8d7b
Steven <steven@ceriously.com>
2023-01-02 17:34:41 -0500

    Add CI (#5)
    Add a test and configure github actions to run CI

---
+++ test/awaken-test.js
@@ -0,0 +1,23 @@
+import test from 'node:test';
+import assert from 'node:assert';
+
+import { awaken } from '../dist/index.js';
+
+test('awaken example', async () => {
+  const initUrl = new URL('https://example.vercel.sh');
+  const results = new Map();
+  let count = 0;
+  const onAwaken = () => { count++ };
+  const start = Date.now();
+  const returnValue = await awaken({ url: initUrl, onAwaken, results });
+  const end = Date.now();
+  const seconds = (end - start) / 1000;
+  assert.strictEqual(returnValue, undefined);
+  assert.strictEqual(count, 2);
+  assert.strictEqual(results.size, 2);
+  assert.ok(seconds < 1, `should be less than 1 seconds, but took ${seconds}`);
+  assert.deepStrictEqual(Array.from(results.values()), [
+    { url: 'https://example.vercel.sh/', status: 200, referer: '' },
+    { url: 'https://examples.vercel.live/', status: 200, referer: 'https://example.vercel.sh/' },
+  ]);
+});
