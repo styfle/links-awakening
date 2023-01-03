f2507eac206afaeb80d6531a1f8a17e33a5734dc
Divlo <contact@divlo.fr>
2023-01-03 17:18:28 +0100

    fix: exit with correct status code (#8)
    Fixes #7
    
    Notes:
    - Added `@types/node` dev dependencies to improve type safety, notably
    for `process` for example, and also verifying that the user gives a URL
    as third argument. It can be out of scope of this PR, so feel free to
    tell me if you want to add that in a separated PR.
    - Added a new test case in `test/cli-test.js`, but it only has the
    "happy-path" scenario where all links are valid. I'm not sure what
    website, we could take for the test, to verify that it exits with status
    code 1 if there are broken links.
    - ESLint, Prettier and editorconfig are not configured on the project, I
    tried to follow the project conventions with the code already available,
    forgive me if I forgot a `;` or semicolon or something. :sweat_smile:
    However, it could be great to integrate these tools to improve
    consistency (could be done in a separate PR).
    
    Co-authored-by: Steven <steven@ceriously.com>

--- src/bin.ts
+++ src/bin.ts
@@ -1,15 +1,19 @@
 #!/usr/bin/env node
 import { awaken, AwakenResult } from './index.js';
 
-// @ts-ignore
-const process = globalThis.process;
 const str = process.argv[2];
+if (!str) {
+  console.error('Usage: links-awakening <url>');
+  process.exit(1);
+}
 const initUrl = new URL(str);
 const results = new Map<string, AwakenResult>();
+let isSuccessful = true;
 const onAwaken = ({ status, url, referer, msg }: AwakenResult) => {
   if (200 <= status && status <= 299) {
     console.log(`✅ ${url}`);
   } else {
+    isSuccessful = false;
     console.log(`❌ ${url} (status: ${msg ?? status}, referer: ${referer})`);
   }
 }
@@ -18,4 +22,5 @@ await awaken({ url: initUrl, onAwaken, results });
 const end = Date.now();
 const seconds = (end - start) / 1000;
 console.log(`Done! Awakened ${results.size} links in ${seconds} seconds`);
-process.exit(0); // TODO: should this exit with 1 if there were errors?
+
+process.exit(isSuccessful ? 0 : 1);
