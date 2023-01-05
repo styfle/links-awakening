8b149807d6108b9e47a7b01247c392532fb5cbd4
Divlo <contact@divlo.fr>
2023-01-05 19:40:16 +0100

    chore: add tooling (#9)
    Following
    https://github.com/styfle/links-awakening/pull/8#issuecomment-1369959986.
    
    Here's the PR to add some tooling to ease future contributions.
    Following https://github.com/vercel/style-guide.
    
    The PR might be hard to review unfortunately, but I didn't do much, I
    basically executed:
    ```sh
    pnpm run lint:prettier
    pnpm run lint:eslint --fix 
    ```
    And fixed manually the few issues that couldn't be resolved
    automatically.
    There are still 2 ESLint warnings, but it seems like they are false
    positive, I couldn't fixed those.
    
    Also I deleted the useless comments in `tsconfig.json` and extended
    `@vercel/style-guide/typescript`.

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+const { resolve } = require('node:path');
+
+const project = resolve(__dirname, 'tsconfig.json');
+
+module.exports = {
+  root: true,
+  extends: [
+    require.resolve('@vercel/style-guide/eslint/node'),
+    require.resolve('@vercel/style-guide/eslint/typescript'),
+  ],
+  parserOptions: {
+    project,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  rules: {
+    'no-console': 'off',
+  },
+};
--- test/cli-test.js
+++ test/cli-test.js
@@ -1,10 +1,10 @@
-import test from 'node:test';
 import assert from 'node:assert';
-import child_process from 'node:child_process';
+import test from 'node:test';
+import childProcess from 'node:child_process';
 
 test('cli example', () => {
   try {
-    child_process.execSync('node ./dist/bin.js https://example.vercel.sh');
+    childProcess.execSync('node ./dist/bin.js https://example.vercel.sh');
   } catch {
     assert.fail('All links should be reachable.');
   }
