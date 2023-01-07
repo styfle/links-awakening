3e58aba464764b61ba83adb4e75456599b288dab
Steven <steven@ceriously.com>
2023-01-06 19:41:23 -0500

    fix: release.config.js should use esm (#12)
    https://github.com/styfle/links-awakening/actions/runs/3859553846/jobs/6579194645#step:10:16

--- release.config.js
+++ release.config.js
@@ -1,5 +1,8 @@
-module.exports = {
+const config = {
   branches: ['main'],
   // eslint-disable-next-line no-template-curly-in-string
   tagFormat: '${version}',
 };
+
+// eslint-disable-next-line import/no-default-export
+export default config;
