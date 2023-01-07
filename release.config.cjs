bdfae65fe2a8e8471644940034408084cb816503
Steven <steven@ceriously.com>
2023-01-06 19:44:19 -0500

    fix: rename to release.config.cjs (#13)
    This reverts commit 3e58aba and uses .cjs instead

--- release.config.cjs
+++ release.config.cjs
@@ -1,8 +1,5 @@
-const config = {
+module.exports = {
   branches: ['main'],
   // eslint-disable-next-line no-template-curly-in-string
   tagFormat: '${version}',
 };
-
-// eslint-disable-next-line import/no-default-export
-export default config;
